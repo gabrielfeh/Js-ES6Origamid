5f4c987e248a885f4ad0a6d8c84a532fa3d9c319
Gabriel Felipe da Silva <gabrierebe@gmail.com>
2023-02-07 15:01:45 -0300

    aulas 09 e 10

---
+++ modulo02/aula09/script.js
@@ -0,0 +1,11 @@
+var idade = 20
+var podeBeber = (idade >= 18) ? "Pode beber" : "Não pode beber"
+console.log(podeBeber)
+
+//----------------------------------
+
+var possuiFaculdade = false
+if(possuiFaculdade)
+    console.log("Possui faculdade.")
+else
+    console.log("Não possui faculdade.")
---
+++ modulo02/aula10/script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function mostrarCarro(){
+    const carro = 'Fusca'
+    console.log(carro)
+}
+
+mostrarCarro()
+
+{
+    var mes = 'Dezembro'
+    console.log(mes);
+}
+console.log(mes)
