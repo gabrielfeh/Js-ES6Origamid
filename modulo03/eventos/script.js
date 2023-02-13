98d8f9aec0c67ce3ff493f6345fb4101e4fb7305
Gabriel Felipe da Silva <gabrierebe@gmail.com>
2023-02-13 09:20:21 -0300

    att

--- modulo03/eventos/script.js
+++ modulo03/eventos/script.js
@@ -1,4 +1,4 @@
-const img = document.querySelector('img')
+/* const img = document.querySelector('img')
 function callback(event){
     console.log(event)
 }
@@ -43,3 +43,70 @@ window.addEventListener('scroll', handleEvent)
 //-----------------------------------------------------
 
 
+
+const imgs = document.querySelectorAll('img')
+
+function handleImg(event){
+    console.log(event.currentTarget)
+}
+
+imgs.forEach((img) => {
+    img.addEventListener('click', handleImg)
+});
+
+
+ */
+
+//Exercícios
+
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+
+const linkInt = document.querySelectorAll('a[href^="#"')
+
+function handleLink(event){
+    event.preventDefault()
+    linkInt.forEach((link)=>{
+        link.classList.remove('ativo')
+    })
+    this.classList.add('ativo')
+    console.log(event)
+}
+
+linkInt.forEach((link) =>{
+    link.addEventListener('click', handleLink)
+})
+
+
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+
+const todosElementos = document.querySelectorAll('body *')
+
+function handleElemento(event){
+    console.log(event.currentTarget.remove())
+
+}
+
+/* todosElementos.forEach((elemento) =>{
+    elemento.addEventListener('click', handleElemento)
+})
+ */
+
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site. 
+
+function handleClickT(event){
+    console.log(event.key)
+    if(event.key === 't'){
+        document.documentElement.classList.toggle('textomaior')
+    }
+}
+
+window.addEventListener('keydown', handleClickT)
